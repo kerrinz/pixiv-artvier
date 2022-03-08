c6bc7ff4362987945172f9095b67e509e9b556cf
kerrinz <1281540128@qq.com>
2022-03-08 17:51:16 +0800

    Fix bookmark/unbookmark the home page can not refresh UI.

--- lib/component/illust_waterfall/illust_waterfall_grid.dart
+++ lib/component/illust_waterfall/illust_waterfall_grid.dart
@@ -9,54 +9,80 @@ import 'package:waterfall_flow/waterfall_flow.dart';
 
 import 'illust_waterfall_card.dart';
 
-class IllustWaterfallGrid extends StatefulWidget {
+class IllustWaterfallGrid extends StatelessWidget {
   final List<CommonIllust> artworkList; // 图片含基本信息的列表
   final Function onLazyLoad; // 触发懒加载（加载更多）的时候调用
   final int? limit; // 列表项的极限数量，为空则表示不限
   final ScrollController? scrollController;
   final ScrollPhysics? physics;
+  final bool isSliver; // 是否为Sliver型组件
 
   const IllustWaterfallGrid(
       {Key? key, required this.artworkList, required this.onLazyLoad, this.limit, this.scrollController, this.physics})
-      : super(key: key);
+      : isSliver = false,
+        super(key: key);
 
-  @override
-  State<StatefulWidget> createState() => IllustWaterfallGridState();
-}
+  const IllustWaterfallGrid.sliver(
+      {Key? key, required this.artworkList, required this.onLazyLoad, this.limit, this.scrollController, this.physics})
+      : isSliver = true,
+        super(key: key);
 
-class IllustWaterfallGridState extends State<IllustWaterfallGrid> {
   @override
   Widget build(BuildContext context) {
-    return WaterfallFlow.builder(
-      padding: EdgeInsets.zero,
-      controller: widget.scrollController,
-      physics: widget.physics,
-      itemCount: widget.artworkList.length + 1,
-      gridDelegate: SliverWaterfallFlowDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        collectGarbage: (List<int> garbages) {
-          // print('collect garbage : $garbages');
-          for (var index in garbages) {
-            final provider = CachedNetworkImageProvider(
-              widget.artworkList[index].imageUrls.medium,
-            );
-            provider.evict();
-          }
-        },
-        viewportBuilder: (int firstIndex, int lastIndex) {
-          // print('viewport : [$firstIndex,$lastIndex]');
-        },
-      ),
-      itemBuilder: (BuildContext context, int index) => _buildItem(context, index),
-    );
+    return isSliver
+        ? SliverWaterfallFlow(
+            gridDelegate: SliverWaterfallFlowDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              collectGarbage: (List<int> garbages) {
+                // print('collect garbage : $garbages');
+                // 内存回收
+                int end = garbages.last;
+                for (int i = garbages.first; i <= end; i++) {
+                  final provider = CachedNetworkImageProvider(
+                    artworkList[i].imageUrls.medium,
+                  );
+                  provider.evict();
+                }
+              },
+              viewportBuilder: (int firstIndex, int lastIndex) {
+                // print('viewport : [$firstIndex,$lastIndex]');
+              },
+            ),
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) => _buildItem(context, index),
+              childCount: artworkList.length + 1,
+            ),
+          )
+        : WaterfallFlow.builder(
+            padding: EdgeInsets.zero,
+            controller: scrollController,
+            physics: physics,
+            itemCount: artworkList.length + 1,
+            gridDelegate: SliverWaterfallFlowDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              collectGarbage: (List<int> garbages) {
+                // print('collect garbage : $garbages');
+                for (var index in garbages) {
+                  final provider = CachedNetworkImageProvider(
+                    artworkList[index].imageUrls.medium,
+                  );
+                  provider.evict();
+                }
+              },
+              viewportBuilder: (int firstIndex, int lastIndex) {
+                // print('viewport : [$firstIndex,$lastIndex]');
+              },
+            ),
+            itemBuilder: (BuildContext context, int index) => _buildItem(context, index),
+          );
   }
 
   Widget _buildItem(BuildContext context, index) {
     // 如果滑动到了表尾加载更多的项
-    if (index == widget.artworkList.length) {
+    if (index == artworkList.length) {
       // 未到列表上限，继续获取数据
-      if (widget.artworkList.length < (widget.limit ?? double.infinity)) {
-        if (widget.artworkList.isNotEmpty) widget.onLazyLoad(); // 列表不为空才获取数据
+      if (artworkList.length < (limit ?? double.infinity)) {
+        if (artworkList.isNotEmpty) onLazyLoad(); // 列表不为空才获取数据
         //加载时显示loading
         return _buildLoading(context);
       } else {
@@ -74,28 +100,32 @@ class IllustWaterfallGridState extends State<IllustWaterfallGrid> {
     return Padding(
       padding: const EdgeInsets.all(4),
       child: IllustWaterfallCard(
-        illust: widget.artworkList[index],
-        isBookmarked: widget.artworkList[index].isBookmarked,
-        onTap: () => widget.artworkList[index].restrict == 2
+        illust: artworkList[index],
+        isBookmarked: artworkList[index].isBookmarked,
+        onTap: () => artworkList[index].restrict == 2
             ? Fluttertoast.showToast(msg: "该图片已被删除或不公开", toastLength: Toast.LENGTH_SHORT, fontSize: 16.0)
             : Navigator.of(context).pushNamed("artworks_detail",
                 arguments: ArtworkDetailModel(
-                    list: widget.artworkList,
+                    list: artworkList,
                     index: index,
                     callback: (int index, bool isBookmark) {
                       // 回调方法，传给详情页
-                      widget.artworkList[index].isBookmarked = isBookmark;
-                      setState(() {});
+                      artworkList[index].isBookmarked = isBookmark;
+                      (context as Element).markNeedsBuild();
                     })),
-        onTapBookmark: () {
-          var item = widget.artworkList[index];
-          postBookmark(item.id.toString(), item.isBookmarked).then((value) {
-            Fluttertoast.showToast(msg: "操作成功", toastLength: Toast.LENGTH_SHORT, fontSize: 16.0);
-            widget.artworkList[index].isBookmarked = !item.isBookmarked;
-            setState(() {});
-          }).onError((error, stackTrace) {
-            Fluttertoast.showToast(msg: "操作失败！$error", toastLength: Toast.LENGTH_SHORT, fontSize: 16.0);
-          });
+        onTapBookmark: () async {
+          var item = artworkList[index];
+          try {
+            bool result = await postBookmark(item.id.toString(), item.isBookmarked);
+            if (result) {
+              Fluttertoast.showToast(msg: "操作成功", toastLength: Toast.LENGTH_SHORT, fontSize: 16.0);
+              artworkList[index].isBookmarked = !item.isBookmarked;
+            } else {
+              throw Exception("http status code is not 200.");
+            }
+          } catch (e) {
+            Fluttertoast.showToast(msg: "操作失败！可能已经${ item.isBookmarked ? "取消" : "" }收藏了", toastLength: Toast.LENGTH_SHORT, fontSize: 16.0);
+          }
         },
       ),
     );
@@ -110,12 +140,7 @@ class IllustWaterfallGridState extends State<IllustWaterfallGrid> {
       isSucceed = await ApiIllusts().addIllustBookmark(illustId: id);
     }
     // 执行结果
-    if (isSucceed) {
-      return true;
-    } else {
-      Future.error("Request bookmark failed!");
-      return false;
-    }
+    return isSucceed;
   }
 
   // 构建循环加载动画
