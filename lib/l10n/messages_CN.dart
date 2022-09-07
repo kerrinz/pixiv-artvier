6a6328688c2164552028f0411d678c71308c6b67
kerrinz <1281540128@qq.com>
2022-09-08 00:21:32 +0800

    feat: auto language partially supported

---
+++ lib/l10n/messages_CN.dart
@@ -0,0 +1,39 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a CN locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, always_declare_return_types
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = MessageLookup();
+
+typedef String MessageIfAbsent(String? messageStr, List<Object>? args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'CN';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function> {
+    "dailyRankings" : MessageLookupByLibrary.simpleMessage("每日排行"),
+    "followers" : MessageLookupByLibrary.simpleMessage("粉丝"),
+    "following" : MessageLookupByLibrary.simpleMessage("关注"),
+    "friends" : MessageLookupByLibrary.simpleMessage("好P友"),
+    "illustrations" : MessageLookupByLibrary.simpleMessage("插画"),
+    "manga" : MessageLookupByLibrary.simpleMessage("漫画"),
+    "more" : MessageLookupByLibrary.simpleMessage("更多"),
+    "navDiscover" : MessageLookupByLibrary.simpleMessage("发现"),
+    "navDynamic" : MessageLookupByLibrary.simpleMessage("动态"),
+    "navHome" : MessageLookupByLibrary.simpleMessage("首页"),
+    "navProfile" : MessageLookupByLibrary.simpleMessage("我的"),
+    "novels" : MessageLookupByLibrary.simpleMessage("小说"),
+    "rankings" : MessageLookupByLibrary.simpleMessage("排行榜"),
+    "recommended" : MessageLookupByLibrary.simpleMessage("为你推荐")
+  };
+}
