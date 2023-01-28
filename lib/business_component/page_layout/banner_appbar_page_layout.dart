3e27f40232f1d6e8ee41a98da7246028c4c21472
yleencc <1281540128@qq.com>
2023-01-28 22:06:51 +0800

    refactor: user detail page

---
+++ lib/business_component/page_layout/banner_appbar_page_layout.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+/// 构建 [AppBar] 的函数
+///
+/// [offset] 垂直方向的偏移量
+typedef AppBarBuilder = Widget Function(double offset);
+
+class BannerAppBarPageLayout extends ConsumerStatefulWidget {
+  const BannerAppBarPageLayout({
+    super.key,
+    required this.appBarBuilder,
+    required this.bannerWidget,
+    required this.bannerHeight,
+    required this.appBarStartBuilderOffset,
+    required this.appBarEndBuilderOffset,
+    required this.body,
+    this.scrollController,
+  });
+
+  final ScrollController? scrollController;
+
+  final AppBarBuilder appBarBuilder;
+
+  /// 封面图区域的Widget，其管理状态可在内部自行实现
+  final Widget bannerWidget;
+
+  /// 主体内容，需要自行添加的padding-top，以避免遮挡banner
+  ///
+  /// （为了更高的自由度，例如自由定制R角）
+  final Widget body;
+
+  /// 封面图高度（仅用于滚动计算，不影响实际组件高度）
+  final double bannerHeight;
+
+  /// AppBar开始实时构建时，所处的垂直滚动位置
+  final double appBarStartBuilderOffset;
+
+  /// AppBar结束实时构建时，所处的垂直滚动位置
+  ///
+  /// （为了减少不必要的rebuild开销，故没有全程调用[appBarBuilder]去构建）
+  final double appBarEndBuilderOffset;
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _BannerAppBarPageLayoutState();
+}
+
+class _BannerAppBarPageLayoutState extends ConsumerState<BannerAppBarPageLayout> {
+  late ScrollController _scrollController;
+
+  /// 是否已经挂载了监听事件
+  bool _hasMountedListener = false;
+
+  /// 背景封面图的高度
+  double get _bannerHeight => widget.bannerHeight;
+
+  /// 用于AppBar构建的滚动位置监听器
+  final ValueNotifier<double> _appBarOffsetNotifier = ValueNotifier<double>(0);
+
+  /// 封面图区域的垂直偏移量
+  final ValueNotifier<double> _bannerffsetNotifier = ValueNotifier<double>(0);
+
+  double get _start => widget.appBarStartBuilderOffset;
+  double get _end => widget.appBarEndBuilderOffset;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (!_hasMountedListener) {
+      _scrollController = PrimaryScrollController.of(context) ?? ScrollController();
+      _scrollController.addListener(_handleScroll);
+      _hasMountedListener = true;
+    }
+  }
+
+  // 滚动事件
+  void _handleScroll() {
+    // 更新AppBar构建所需要的偏移量
+    double offset = _scrollController.offset;
+    if (offset >= _end) {
+      if (_appBarOffsetNotifier.value != _end) _appBarOffsetNotifier.value = _end;
+    } else if (offset <= _start) {
+      if (_appBarOffsetNotifier.value != _start) _appBarOffsetNotifier.value = _start;
+    } else {
+      _appBarOffsetNotifier.value = offset;
+    }
+    // 更新banner的位置
+    if (offset > _bannerHeight) {
+      if (_bannerffsetNotifier.value != _bannerHeight) _bannerffsetNotifier.value = _bannerHeight;
+    } else {
+      _bannerffsetNotifier.value = offset;
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        /// 顶部封面图
+        ValueListenableBuilder<double>(
+            valueListenable: _bannerffsetNotifier,
+            builder: (_, value, __) {
+              return Transform.translate(
+                offset: Offset(0, -value),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: _bannerHeight,
+                  child: widget.bannerWidget,
+                ),
+              );
+            }),
+        // 主体内容
+        widget.body,
+        // 固定顶部的AppBar
+        Positioned(
+          top: 0,
+          left: 0,
+          right: 0,
+          child: ValueListenableBuilder<double>(
+              valueListenable: _appBarOffsetNotifier,
+              builder: (_, value, __) {
+                return widget.appBarBuilder(value);
+              }),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/pages/user/detail/widget/user_panel_widget.dart
@@ -0,0 +1,228 @@
+import 'package:extended_image/extended_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:pixgem/component/text/collapsible_text.dart';
+import 'package:pixgem/config/constants.dart';
+import 'package:pixgem/config/enums.dart';
+import 'package:pixgem/l10n/localization_intl.dart';
+import 'package:pixgem/model_response/user/user_detail.dart';
+import 'package:pixgem/pages/artwork/detail/widgets/user_follow_button.dart';
+import 'package:pixgem/pages/user/detail/provider/user_detail_provider.dart';
+import 'package:pixgem/routes.dart';
+
+/// 用户详情页的用户面板组件
+class UserDetailPageUserPanelWidget extends ConsumerWidget {
+  const UserDetailPageUserPanelWidget({
+    super.key,
+    required this.userId,
+    required this.userName,
+    required this.avatarUrl,
+    required this.avatarDiameter,
+  });
+
+  final String userId;
+
+  final String userName;
+
+  final String avatarUrl;
+
+  /// 头像的直径
+  final double avatarDiameter;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    var i10n = LocalizationIntl.of(context);
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              // 左侧的头像
+              Padding(
+                padding: const EdgeInsets.only(right: 16),
+                child: Container(
+                  width: avatarDiameter,
+                  height: avatarDiameter,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black12, width: 1),
+                    borderRadius: const BorderRadius.all(Radius.circular(80)),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: ExtendedNetworkImageProvider(
+                        avatarUrl,
+                        headers: {"Referer": CONSTANTS.referer},
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              // 右侧
+              Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 2.0),
+                    child: Consumer(
+                      builder: (_, ref, __) => ref.watch(userDetailProvider(userId)).when(
+                            error: (_, __) => Container(),
+                            loading: () => Container(),
+                            data: (detail) => Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                // 右侧的水平分栏信息
+                                _rightHorizontalColumnsWidget(context, detail),
+                                // 右侧的关注按钮
+                                _followButton(detail),
+                              ],
+                            ),
+                          ),
+                    ),
+                  )),
+            ],
+          ),
+          // 用户名
+          Consumer(builder: (_, ref, __) {
+            return ref.watch(userDetailProvider(userId)).when(
+                  error: (_, __) => _userNameWidget(userName),
+                  loading: () => _userNameWidget(userName),
+                  data: (detail) => _userNameWidget(detail.user.name),
+                );
+          }),
+          // UID信息
+          GestureDetector(
+            onTap: () {
+              // 点击复制UID
+              Clipboard.setData(ClipboardData(text: userId)).then(
+                (value) => Fluttertoast.showToast(
+                  msg: i10n.copiedToClipboard,
+                  toastLength: Toast.LENGTH_SHORT,
+                  fontSize: 16.0,
+                ),
+              );
+            },
+            child: Row(
+              children: [
+                Text("UID: $userId", style: const TextStyle(fontSize: 12), overflow: TextOverflow.ellipsis),
+                const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 2.0, vertical: 2.0), child: Icon(Icons.copy, size: 12)),
+              ],
+            ),
+          ),
+          // 个人介绍
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 2.0, vertical: 2.0),
+            child: Consumer(
+              builder: (_, ref, __) {
+                return ref.watch(userDetailProvider(userId)).when(
+                      data: (data) => CollapsibleText(
+                        text: data.user.comment ?? "",
+                        style: const TextStyle(fontSize: 14),
+                        collapsedMaxLine: 3,
+                        isCollapsedInitially: true,
+                        isSelectable: true,
+                        buttonAxisAlignment: MainAxisAlignment.end,
+                      ),
+                      error: (_, __) => Container(),
+                      loading: () => Container(),
+                    );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  /// 用户名组件
+  Widget _userNameWidget(String name) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 2.0, right: 2.0, top: 8.0, bottom: 2.0),
+      child: SelectableText(
+        name,
+        style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+      ),
+    );
+  }
+
+  /// 右侧的水平分栏信息
+  Widget _rightHorizontalColumnsWidget(BuildContext context, UserDetail detail) {
+    var i10n = LocalizationIntl.of(context);
+    const TextStyle numberTextStyle = TextStyle(fontSize: 18, fontWeight: FontWeight.bold);
+    // 性别、关注、好P友的统计信息
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // 性别
+        Expanded(
+          flex: 1,
+          child: Column(
+            children: [
+              Text(i10n.gender, style: Theme.of(context).textTheme.labelSmall),
+              Builder(builder: (_) {
+                var gender = detail.profile.gender;
+                switch (gender) {
+                  case "male":
+                    return const Icon(Icons.male, color: Colors.blue, size: 22);
+                  case "female":
+                    return Icon(Icons.female, color: Colors.pink.shade300.withOpacity(0.75), size: 22);
+                  case "unknown":
+                    return Icon(Icons.transgender_outlined, color: Colors.blueGrey.withOpacity(0.75), size: 22);
+                  default:
+                    return const Text("-", style: numberTextStyle);
+                }
+              }),
+            ],
+          ),
+        ),
+        // 关注数
+        Expanded(
+          flex: 1,
+          child: GestureDetector(
+            onTap: () => Navigator.of(context).pushNamed(RouteNames.userFollowing.name, arguments: userId),
+            child: Column(
+              children: [
+                Text(i10n.following, style: Theme.of(context).textTheme.labelSmall),
+                Text(detail.profile.totalFollowUsers.toString(), style: numberTextStyle),
+              ],
+            ),
+          ),
+        ),
+        // 好P友数
+        Expanded(
+          flex: 1,
+          child: Column(
+            children: [
+              Text(i10n.friends, style: Theme.of(context).textTheme.labelSmall),
+              Text(detail.profile.totalMypixivUsers.toString(), style: numberTextStyle),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  /// 关注按钮
+  Widget _followButton(UserDetail detail) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 4.0),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: UserFollowButton(
+                userId: userId,
+                followState: detail.user.isFollowed ? UserFollowState.followed : UserFollowState.notFollow,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
