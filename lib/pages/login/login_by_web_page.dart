df3f54054de4aad9eac237d26f3b4f568df55a2a
yleencc <1281540128@qq.com>
2024-03-31 15:23:48 +0800

    chore: update webview_flutter pub package, remove unused page

--- lib/pages/login/login_by_web_page.dart
+++ lib/pages/login/login_by_web_page.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:artvier/global/provider/current_account_provider.dart';
@@ -15,11 +13,33 @@ class LoginWebPage extends BaseStatefulPage {
 }
 
 class _LoginWebState extends BasePageState<LoginWebPage> {
+  final WebViewController _webViewController = WebViewController();
+
   @override
   void initState() {
     super.initState();
-    // WebView使用Hybrid composition模式以避免安全键盘无法弹出的问题
-    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+    String url = ref.read(globalCurrentAccountProvider.notifier).getLoginWebViewUrl();
+    _webViewController
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse(url))
+      ..setNavigationDelegate(NavigationDelegate(onNavigationRequest: (NavigationRequest request) async {
+        if (request.url.startsWith("pixiv://account/")) {
+          // 拦截，拿到code
+          String? code = Uri.parse(request.url).queryParameters["code"];
+          if (code != null) {
+            // OAuth登录
+            ref.read(globalCurrentAccountProvider.notifier).oAuthLogin(code).then((value) {
+              Navigator.pushNamedAndRemoveUntil(context, RouteNames.mainNavigation.name, (route) => false);
+            }).catchError((onError) {
+              // TODO: 待处理登录失败的逻辑
+            });
+          } else {
+            //
+          }
+          return NavigationDecision.prevent;
+        }
+        return NavigationDecision.navigate;
+      }));
   }
 
   @override
@@ -28,51 +48,9 @@ class _LoginWebState extends BasePageState<LoginWebPage> {
       appBar: AppBar(
         title: const Text('使用网页登录'),
       ),
-      body: WebView(
-        javascriptMode: JavascriptMode.unrestricted,
-        onPageFinished: (url) {},
-        onWebViewCreated: (controller) {},
-        initialUrl: ref.read(globalCurrentAccountProvider.notifier).getLoginWebViewUrl(),
-        navigationDelegate: (NavigationRequest request) {
-          if (request.url.startsWith("pixiv://account/")) {
-            // 拦截，拿到code
-            String? code = Uri.parse(request.url).queryParameters["code"];
-            if (code != null) {
-              // OAuth登录
-              oAuthLogin(code).then((value) {
-                Navigator.pushNamedAndRemoveUntil(context, RouteNames.mainNavigation.name, (route) => false);
-              }).catchError((onError) {
-                // 待处理登录失败的逻辑
-              });
-            } else {
-              //
-            }
-            return NavigationDecision.prevent;
-          }
-          return NavigationDecision.navigate;
-        },
-        javascriptChannels: <JavascriptChannel>{
-          JavascriptChannel(
-              name: "rnm",
-              onMessageReceived: (JavascriptMessage message) {
-                try {
-                  // var value = json.decode(message.message);
-                  // var id = value["_id"];
-                  // var token = value["_token"];
-                  // var cookie = value["_cookie"];
-                  // AccountStorage.saveCurrentAccountProfile(account: new Account.create(userId: id, token: token, cookie: cookie));
-                  // print(value["_id"]);
-                } catch (e) {
-                  //
-                }
-              }),
-        },
+      body: WebViewWidget(
+        controller: _webViewController,
       ),
     );
-  }
-
-  // OAuth2.1登录方式
-  Future oAuthLogin(code) async {
-    ref.read(globalCurrentAccountProvider.notifier).oAuthLogin(code);
   }
 }
