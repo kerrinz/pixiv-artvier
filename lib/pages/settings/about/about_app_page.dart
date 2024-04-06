13468cfb2bb476969c55b90fd843aa676b5e8f9a
kerrinz <1281540128@qq.com>
2024-04-06 19:08:30 +0800

    feat: update about app page

--- lib/pages/settings/about/about_app_page.dart
+++ lib/pages/settings/about/about_app_page.dart
@@ -1,4 +1,7 @@
 import 'package:artvier/base/base_page.dart';
+import 'package:artvier/component/perference/perference_group.dart';
+import 'package:artvier/component/perference/perference_item.dart';
+import 'package:artvier/pages/settings/about/provider/about_app_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -11,12 +14,48 @@ class AboutAppPage extends ConsumerStatefulWidget {
 
 class _AboutAppPageState extends BasePageState<AboutAppPage> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(i10n.aboutApp),
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        physics: const AlwaysScrollableScrollPhysics(parent: BouncingScrollPhysics()),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height - kToolbarHeight - MediaQuery.of(context).padding.top,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 12),
+            child: Column(
+              children: [
+                PerferenceGroup(
+                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                  items: [
+                    PerferenceItem(
+                      text: const Text("App name"),
+                      value: SelectableText(
+                        ref.watch(packageInfoProvider).whenOrNull(data: (data) => data.appName) ?? "",
+                        style: textTheme.bodyMedium?.copyWith(color: Theme.of(context).colorScheme.secondary),
+                      ),
+                    ),
+                    PerferenceItem(
+                      text: const Text("Version"),
+                      value: SelectableText(
+                        ref.watch(packageInfoProvider).whenOrNull(data: (data) => data.version) ?? "",
+                        style: textTheme.bodyMedium?.copyWith(color: Theme.of(context).colorScheme.secondary),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
