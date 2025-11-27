1545295de0d930133e04a735e1a14b7e427bb28d
kerrinz <1281540128@qq.com>
2025-11-28 00:30:52 +0800

    fix: scrollable of check update page

--- lib/pages/settings/check_update/check_update.dart
+++ lib/pages/settings/check_update/check_update.dart
@@ -30,130 +30,134 @@ class _CheckUpdatePageState extends BasePageState<CheckUpdatePage> {
       appBar: AppBar(
         title: Text(l10n.softwareUpdate),
       ),
-      body: SingleChildScrollView(
-        physics: const AlwaysScrollableScrollPhysics(parent: BouncingScrollPhysics()),
-        child: SizedBox(
-          height: MediaQuery.sizeOf(context).height -
-              MediaQuery.paddingOf(context).top -
-              MediaQuery.paddingOf(context).bottom,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 12),
-            child: Column(
-              children: [
-                PerferenceGroup(
-                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-                  items: [
-                    PerferenceItem(
-                      text: Text(l10n.currentVersion),
-                      value: SelectableText(
-                        ref.watch(packageInfoProvider).whenOrNull(data: (data) => data.version.split('-').first) ?? "-",
-                        style: textTheme.bodyMedium?.copyWith(color: Theme.of(context).colorScheme.secondary),
+      body: SizedBox(
+        height: MediaQuery.sizeOf(context).height -
+            MediaQuery.paddingOf(context).top -
+            MediaQuery.paddingOf(context).bottom,
+        child: SingleChildScrollView(
+          physics: const AlwaysScrollableScrollPhysics(parent: BouncingScrollPhysics()),
+          child: SizedBox(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 12),
+              child: Column(
+                children: [
+                  PerferenceGroup(
+                    padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                    items: [
+                      PerferenceItem(
+                        text: Text(l10n.currentVersion),
+                        value: SelectableText(
+                          ref.watch(packageInfoProvider).whenOrNull(data: (data) => data.version.split('-').first) ??
+                              "-",
+                          style: textTheme.bodyMedium?.copyWith(color: Theme.of(context).colorScheme.secondary),
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: colorScheme.surface,
-                    borderRadius: const BorderRadius.all(Radius.circular(10)),
+                    ],
                   ),
-                  child: Consumer(
-                    builder: (context, ref, child) {
-                      final result = ref.watch(globalLastVersionProvider);
-                      final currentVersion =
-                          ref.watch(packageInfoProvider).whenOrNull(data: (data) => data.version.split('-').first);
-                      return result.when(
-                          data: (data) {
-                            if (currentVersion != null && data.tagName != currentVersion) {
-                              // 有新版本（也可能在等待加载当前版本号）
-                              return Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Row(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Padding(
-                                        padding: const EdgeInsets.only(right: 6, top: 0, bottom: 4),
-                                        child: Text(
-                                          data.tagName,
-                                          style: textTheme.headlineMedium?.copyWith(color: colorScheme.primary),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                    padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: colorScheme.surface,
+                      borderRadius: const BorderRadius.all(Radius.circular(10)),
+                    ),
+                    child: Consumer(
+                      builder: (context, ref, child) {
+                        final result = ref.watch(globalLastVersionProvider);
+                        final currentVersion =
+                            ref.watch(packageInfoProvider).whenOrNull(data: (data) => data.version.split('-').first);
+                        return result.when(
+                            data: (data) {
+                              if (currentVersion != null && data.tagName != currentVersion) {
+                                // 有新版本（也可能在等待加载当前版本号）
+                                return Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Padding(
+                                          padding: const EdgeInsets.only(right: 6, top: 0, bottom: 4),
+                                          child: Text(
+                                            data.tagName,
+                                            style: textTheme.headlineMedium?.copyWith(color: colorScheme.primary),
+                                          ),
+                                        ),
+                                        MyBadge(
+                                          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+                                          color: Colors.redAccent,
+                                          child:
+                                              Text("NEW", style: textTheme.labelSmall?.copyWith(color: Colors.white)),
+                                        ),
+                                      ],
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.symmetric(vertical: 8),
+                                      child: Text(l10n.downloadLink, style: textTheme.titleMedium),
+                                    ),
+                                    for (final asset in data.assets)
+                                      _buildBulletLine(Padding(
+                                        padding: const EdgeInsets.symmetric(vertical: 4),
+                                        child: SelectableText(
+                                          onTap: () => showOpenLinkDialog(context, asset.browserDownloadUrl),
+                                          asset.browserDownloadUrl,
+                                          style: textTheme.bodySmall
+                                              ?.copyWith(color: Theme.of(context).colorScheme.secondary),
+                                        ),
+                                      )),
+                                    Padding(
+                                      padding: const EdgeInsets.only(top: 8),
+                                      child: SelectionArea(
+                                        child: MarkdownBody(
+                                          data: data.body,
+                                          selectable: false,
                                         ),
                                       ),
-                                      MyBadge(
-                                        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
-                                        color: Colors.redAccent,
-                                        child: Text("NEW", style: textTheme.labelSmall?.copyWith(color: Colors.white)),
-                                      ),
-                                    ],
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(vertical: 8),
-                                    child: Text(l10n.downloadLink, style: textTheme.titleMedium),
-                                  ),
-                                  for (final asset in data.assets)
-                                    _buildBulletLine(Padding(
-                                      padding: const EdgeInsets.symmetric(vertical: 4),
-                                      child: SelectableText(
-                                        onTap: () => showOpenLinkDialog(context, asset.browserDownloadUrl),
-                                        asset.browserDownloadUrl,
-                                        style: textTheme.bodySmall
-                                            ?.copyWith(color: Theme.of(context).colorScheme.secondary),
-                                      ),
-                                    )),
-                                  Padding(
-                                    padding: const EdgeInsets.only(top: 8),
-                                    child: SelectionArea(
-                                      child: MarkdownBody(
-                                        data: data.body,
-                                        selectable: false,
+                                    ),
+                                  ],
+                                );
+                              }
+                              // 当前已经是最新版本
+                              return Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                                child: Text(l10n.currentIsLatestVersion, textAlign: TextAlign.center),
+                              );
+                            },
+                            error: (error, stackTrace) {
+                              final exception = error as DioException;
+                              final statusCode = exception.response?.statusCode;
+                              if (statusCode == 403) {
+                                // API访问次数超过限制
+                                return Wrap(
+                                  alignment: WrapAlignment.start,
+                                  runAlignment: WrapAlignment.start,
+                                  crossAxisAlignment: WrapCrossAlignment.start,
+                                  direction: Axis.horizontal,
+                                  children: [
+                                    Text(l10n.checkUpdateLimitForGithubApi),
+                                    GestureDetector(
+                                      onTap: () => showOpenLinkDialog(context, CONSTANTS.app_repo_release_url),
+                                      child: Text(
+                                        CONSTANTS.app_repo_release_url,
+                                        style: textTheme.bodyMedium?.copyWith(
+                                            color: Theme.of(context).colorScheme.secondary,
+                                            decoration: TextDecoration.underline),
                                       ),
                                     ),
-                                  ),
-                                ],
+                                  ],
+                                );
+                              }
+                              return RequestLoadingFailed(
+                                onRetry: () => ref.read(globalLastVersionProvider.notifier).reload(),
                               );
-                            }
-                            // 当前已经是最新版本
-                            return Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 8.0),
-                              child: Text(l10n.currentIsLatestVersion, textAlign: TextAlign.center),
-                            );
-                          },
-                          error: (error, stackTrace) {
-                            final exception = error as DioException;
-                            final statusCode = exception.response?.statusCode;
-                            if (statusCode == 403) {
-                              // API访问次数超过限制
-                              return Wrap(
-                                alignment: WrapAlignment.start,
-                                runAlignment: WrapAlignment.start,
-                                crossAxisAlignment: WrapCrossAlignment.start,
-                                direction: Axis.horizontal,
-                                children: [
-                                  Text(l10n.checkUpdateLimitForGithubApi),
-                                  GestureDetector(
-                                    onTap: () => showOpenLinkDialog(context, CONSTANTS.app_repo_release_url),
-                                    child: Text(
-                                      CONSTANTS.app_repo_release_url,
-                                      style: textTheme.bodyMedium?.copyWith(
-                                          color: Theme.of(context).colorScheme.secondary,
-                                          decoration: TextDecoration.underline),
-                                    ),
-                                  ),
-                                ],
-                              );
-                            }
-                            return RequestLoadingFailed(
-                              onRetry: () => ref.read(globalLastVersionProvider.notifier).reload(),
-                            );
-                          },
-                          loading: () => const RequestLoading());
-                    },
-                  ),
-                )
-              ],
+                            },
+                            loading: () => const RequestLoading());
+                      },
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
